815032cf76ff5811802ed9b597f3a8142c13b6e5
Arthur Aguiar <95924380+devarthuraguiar@users.noreply.github.com>
2023-06-16 03:13:24 -0300

    Aula 3 concluída Adicionado footer e animações de texto e disco

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import "./Video.css"
-
+import VideoFooter from './components/footer/VideoFooter'
 function Video() {
   
     const videoRef = useRef(null)
@@ -26,9 +26,9 @@ function Video() {
             onClick={handleStart}
             loop
             src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-        >
-
-        </video>
+        ></video>
+        {/* Sidebar */}
+        <VideoFooter />
     </div>
   )
 }
